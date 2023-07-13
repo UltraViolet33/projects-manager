4a54177240bb9a85881f419729317b4b6ead91e3
Ulysse Valdenaire <93035200+UltraViolet33@users.noreply.github.com>
2023-07-13 16:47:45 +0200

    add db name debug

--- core/connection/config.php
+++ core/connection/config.php
@@ -4,4 +4,13 @@ define('DB_TYPE', 'mysql');
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-define('DB_NAME', 'projects-manager-test');
+
+
+define("DEBUG", true);
+
+
+if (DEBUG) {
+    define('DB_NAME', 'projects-manager-debug');
+} else {
+    define('DB_NAME', 'projects-manager-test');
+}

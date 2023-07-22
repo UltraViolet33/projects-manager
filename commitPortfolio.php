e27666cbf3c8e20d6e94afdf9d5dcbb0b6f58d19
Ulysse Valdenaire <93035200+UltraViolet33@users.noreply.github.com>
2023-07-22 16:33:16 +0200

    delete useless files

--- commitPortfolio.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once("./inc/header.php");
-$project->commitPortfolio();
-header("Location: portfolioProjects.php");
-die;
-?>
-
--- core/classes/pushPortfolio.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/bash
-
-
-
-cp ./core/data/projects.json ../Github-Portofolio/projects.json
-cd ../Github-Portofolio/
-git add *
-git commit -m "add a project"
-git push origin master

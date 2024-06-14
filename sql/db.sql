a2698c90096242844afce995ff800beacc0066b4
Ultraviolet33 <93035200+UltraViolet33@users.noreply.github.com>
2024-06-14 15:37:51 +0200

    export db sql

--- sql/db.sql
+++ sql/db.sql
@@ -25,9 +25,35 @@ CREATE TABLE IF NOT EXISTS `categories` (
   `name` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id_category`) USING BTREE,
   UNIQUE KEY `name` (`name`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=latin1;
 
--- Listage des données de la table projects-manager-debug.categories : ~0 rows (environ)
+-- Les données exportées n'étaient pas sélectionnées.
+
+-- Listage de la structure de table projects-manager-debug. portfolios
+CREATE TABLE IF NOT EXISTS `portfolios` (
+  `id_portfolio` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) DEFAULT NULL,
+  `category_id` int DEFAULT NULL,
+  KEY `FK__categories` (`category_id`),
+  KEY `id_portfolio` (`id_portfolio`),
+  CONSTRAINT `FK__categories` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id_category`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Les données exportées n'étaient pas sélectionnées.
+
+-- Listage de la structure de table projects-manager-debug. portfolio_projects
+CREATE TABLE IF NOT EXISTS `portfolio_projects` (
+  `id_portfolio_project` int NOT NULL AUTO_INCREMENT,
+  `project_id` int DEFAULT NULL,
+  `portfolio_id` int DEFAULT NULL,
+  PRIMARY KEY (`id_portfolio_project`),
+  KEY `FK_portfolio_projects_projects` (`project_id`),
+  KEY `FK_portfolio_projects_portfolios` (`portfolio_id`),
+  CONSTRAINT `FK_portfolio_projects_portfolios` FOREIGN KEY (`portfolio_id`) REFERENCES `portfolios` (`id_portfolio`),
+  CONSTRAINT `FK_portfolio_projects_projects` FOREIGN KEY (`project_id`) REFERENCES `projects` (`id_project`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Les données exportées n'étaient pas sélectionnées.
 
 -- Listage de la structure de table projects-manager-debug. projects
 CREATE TABLE IF NOT EXISTS `projects` (
@@ -38,21 +64,20 @@ CREATE TABLE IF NOT EXISTS `projects` (
   `status` tinyint(1) NOT NULL DEFAULT '0',
   `github_link` varchar(100) DEFAULT NULL,
   `priority` tinyint NOT NULL DEFAULT '0',
-  `github_portfolio` tinyint NOT NULL DEFAULT '0',
   PRIMARY KEY (`id_project`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=115 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=120 DEFAULT CHARSET=latin1;
 
--- Listage des données de la table projects-manager-debug.projects : ~0 rows (environ)
+-- Les données exportées n'étaient pas sélectionnées.
 
 -- Listage de la structure de table projects-manager-debug. projects_categories
 CREATE TABLE IF NOT EXISTS `projects_categories` (
   `id_project` int NOT NULL,
-  `id_categorie` int NOT NULL,
-  PRIMARY KEY (`id_project`,`id_categorie`) USING BTREE,
-  KEY `id_categorie` (`id_categorie`) USING BTREE
+  `id_category` int NOT NULL,
+  PRIMARY KEY (`id_project`,`id_category`) USING BTREE,
+  KEY `id_categorie` (`id_category`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Listage des données de la table projects-manager-debug.projects_categories : ~0 rows (environ)
+-- Les données exportées n'étaient pas sélectionnées.
 
 -- Listage de la structure de table projects-manager-debug. projects_techs
 CREATE TABLE IF NOT EXISTS `projects_techs` (
@@ -64,77 +89,16 @@ CREATE TABLE IF NOT EXISTS `projects_techs` (
   CONSTRAINT `FK_projects_techs_techs` FOREIGN KEY (`id_tech`) REFERENCES `techs` (`id_tech`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Listage des données de la table projects-manager-debug.projects_techs : ~0 rows (environ)
+-- Les données exportées n'étaient pas sélectionnées.
 
 -- Listage de la structure de table projects-manager-debug. techs
 CREATE TABLE IF NOT EXISTS `techs` (
   `id_tech` int unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id_tech`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
 
--- Listage des données de la table projects-manager-debug.techs : ~0 rows (environ)
-
-
--- Listage de la structure de la base pour projects-manager-prod
-CREATE DATABASE IF NOT EXISTS `projects-manager-prod` /*!40100 DEFAULT CHARACTER SET latin1 */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `projects-manager-prod`;
-
--- Listage de la structure de table projects-manager-prod. categories
-CREATE TABLE IF NOT EXISTS `categories` (
-  `id_category` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`id_category`) USING BTREE,
-  UNIQUE KEY `name` (`name`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=latin1;
-
--- Listage des données de la table projects-manager-prod.categories : ~0 rows (environ)
-
--- Listage de la structure de table projects-manager-prod. projects
-CREATE TABLE IF NOT EXISTS `projects` (
-  `id_project` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) NOT NULL,
-  `description` text,
-  `created_at` date NOT NULL,
-  `status` tinyint(1) NOT NULL DEFAULT '0',
-  `github_link` varchar(100) DEFAULT NULL,
-  `priority` tinyint NOT NULL DEFAULT '0',
-  `github_portfolio` tinyint NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id_project`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=115 DEFAULT CHARSET=latin1;
-
--- Listage des données de la table projects-manager-prod.projects : ~0 rows (environ)
-
--- Listage de la structure de table projects-manager-prod. projects_categories
-CREATE TABLE IF NOT EXISTS `projects_categories` (
-  `id_project` int NOT NULL,
-  `id_categorie` int NOT NULL,
-  PRIMARY KEY (`id_project`,`id_categorie`) USING BTREE,
-  KEY `id_categorie` (`id_categorie`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- Listage des données de la table projects-manager-prod.projects_categories : ~0 rows (environ)
-
--- Listage de la structure de table projects-manager-prod. projects_techs
-CREATE TABLE IF NOT EXISTS `projects_techs` (
-  `id_tech` int unsigned DEFAULT NULL,
-  `id_project` int DEFAULT NULL,
-  KEY `FK_projects_techs_projects` (`id_project`),
-  KEY `FK_projects_techs_techs` (`id_tech`),
-  CONSTRAINT `FK_projects_techs_projects` FOREIGN KEY (`id_project`) REFERENCES `projects` (`id_project`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `FK_projects_techs_techs` FOREIGN KEY (`id_tech`) REFERENCES `techs` (`id_tech`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- Listage des données de la table projects-manager-prod.projects_techs : ~0 rows (environ)
-
--- Listage de la structure de table projects-manager-prod. techs
-CREATE TABLE IF NOT EXISTS `techs` (
-  `id_tech` int unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id_tech`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
-
--- Listage des données de la table projects-manager-prod.techs : ~0 rows (environ)
+-- Les données exportées n'étaient pas sélectionnées.
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
